cc3e8e3fefca70d642323ef1b34979120a625509
Alexander Burgos <axb19c@acu.edu>
2021-10-24 16:21:40 -0500

    Adds config file dependency.

--- portfolio_site/index.js
+++ portfolio_site/index.js
@@ -3,9 +3,14 @@ const chalk = require('chalk');//adds color to console messages. (npm install ch
 const debug = require('debug')('app');//middleware that helps with debbuging. (npm install debug)
 const morgan = require('morgan');//middleware that helps to output whats happening with your app. (npm install morgan)
 const path = require('path');
+const config = require('./config.js')
 
 const app = express();
-const port = 3000;
+
+/* SETTINGS */
+app.set('views', './src/views');//sets the path for your views.
+app.set('view engine', 'ejs');//sets your template engine that renders html.
+app.use(express.static(path.join(__dirname, '/public')));
 
 /* ROUTERS */
 const resumeRouter = require('./src/routers/resumeRouter');
@@ -17,16 +22,11 @@ app.use(express.urlencoded({extended:true}));//Parses encoded URLs.
 
 app.use('/resume', resumeRouter);
 
-/* SETTINGS */
-app.set('views', './src/views');//sets the path for your views.
-app.set('view engine', 'ejs');//sets your template engine that renders html.
-app.use(express.static(path.join(__dirname, '/public')));
-
 /* DEFAULT LANDING PAGE */
 app.get('/', (request, response) => {
     response.render('index');
 });
 
-app.listen(port, () =>{
-    debug(`Listening on port ${chalk.green(port)}`);//Sends a message when when app starts
+app.listen(config.app.port, () =>{
+    debug(`Listening on port ${chalk.green(config.app.port)}`);//Sends a message when when app starts
 });
