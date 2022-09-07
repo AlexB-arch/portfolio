109fb898e58357135f37a7bc2435d72718e7c2b5
AlexB <axb19c@acu.edu>
2022-09-06 20:55:51 -0500

    Adds Java

---
+++ Java/maven/Calculator_start/src/main/java/Calculator.java
@@ -0,0 +1,19 @@
+/**
+ * Calculator
+ * do some maths
+ */
+public class Calculator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Calculator! AMA." );
+    }
+    
+    public int add (int x, int y)
+    {
+	return x+y;
+    }
+    
+    // add methods here    
+    
+}
