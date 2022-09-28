21d7db25ae5c08eca38a52a95673aad422bc5546
AlexB <axb19c@acu.edu>
2022-09-27 23:21:05 -0500

    Finished the code part of test 1

--- R/test1.r
+++ R/test1.r
@@ -3,7 +3,7 @@
 
 # 1
 # Extracts data from file and separates by commas into two columns.
-peopleData <- read.delim("377124.txt", header = TRUE, sep= ",")
+peopleData <- read.delim("377124.txt", header = TRUE, sep = ",")
 
 # Mean
 meanMales <- mean(peopleData$MALE)
@@ -51,11 +51,61 @@ iqrFemale <- q3Female - q1Female
 #iqrMale2 <- IQR(peopleData$MALE)
 #iqrFemale2 <- IQR(peopleData$FEMALE)
 
-b <- boxplot(peopleData, horizontal=T, col=c("red", "blue"), pch=16, boxwex=.8, names=c("Males", "Females"))
+cholesterolBoxplot <- boxplot(peopleData, 
+    horizontal = T, 
+    col = c("red", "blue"), 
+    pch = 16, 
+    boxwex = .8, 
+    names = c("Males", "Females"))
 
 ##############################################################################
 
 # 2
 temps <- read.delim("377183.txt", header = TRUE)
 
-h <- hist(temps$Temp, breaks=seq(25.5, 35.5, 1), main="Everglades Temp. in Celcius", xlab="Temperature", ylab="Frequency", col="blue", border="black", xlim=c(25.5, 35.5), ylim=c(0, 20), xaxp=c(25.5, 35.5, 5), labels=T)
+tempsHistogram <- hist(temps$Temp, 
+    breaks = 9, 
+    main = "Everglades Temp. in Celcius", 
+    xlab = "Temperature", 
+    ylab = "Frequency", 
+    col = "blue", 
+    border = "black", 
+    xlim = c(25.5, 35.5), 
+    ylim = c(0, 20), 
+    xaxp = c(25.5, 35.5, 5), 
+    labels = T)
+
+# 3
+# 3a
+carsA <- round((dbinom(0:5, 15, 0.3)), 4)
+
+# 3b
+carsB <- round(sum(dbinom(5:10, 15, 0.3)), 4)
+
+# 3c
+carsC <- round(sum(dbinom(10:15, 15, 0.3)), 4)
+
+# 3d
+carsDMean <- round(25 * (1 - .3), 4)
+carsDSD <- round(sqrt((25 * .3 * (1 - .3))), 4)
+
+# 3e
+carsE <- round(pnorm(carsDMean + carsDSD, carsDMean, carsDSD) - pnorm(carsDMean - carsDSD, carsDMean, carsDSD), 4)
+ 
+ #4
+ wpmMean <- 60
+ wpmSD <- 15
+
+# 4a
+ wpmA <- round(pnorm(55, wpmMean, wpmSD), 4)
+
+# 4b
+ wpmB <- round(pnorm(70, wpmMean, wpmSD) - pnorm(60, wpmMean, wpmSD), 4)
+
+# 4c
+wpmC <- round(1 - pnorm(85, wpmMean, wpmSD), 4)
+
+# 4d
+wpmD <- 1 - pnorm(108, wpmMean, wpmSD)
+
+wpmE <- ((1 - pnorm(75, wpmMean, wpmSD)) + (1 - pnorm(75, wpmMean, wpmSD)))
