1a2aa1bc3808e627097167b8e64af04900c041a4
AlexB <axb19c@acu.edu>
2022-10-14 11:36:35 -0500

    Checkpoint

---
+++ R/277-hw13.r
@@ -0,0 +1,47 @@
+#1. Suppose a set 𝐴 has exactly 3003 subsets with 6 elements. What is the cardinality of 𝐴.
+
+
+#2. How many 16-digit binary strings contain exactly six 1s. (Examples of such strings include
+#0110000111010000 and 0000110101011000.)
+
+#3. Calculate: 𝑋 ∈2{0,1,2,3,4,5,6,7,8,9} : |𝑋| <4
+
+#4. This problem concerns lists of length 6 made from the letters A, B, C, D, E, F without
+#repetition. How many such lists have the property that D comes before the A.
+
+#5. How many positive 10-digit integers contain no 0s and exactly three 6s?
+
+#6. Use the binomial theorem to find the coefficient of 𝑝8 in (2𝑝 −5)13.
+
+#7. Twenty people attend a party. If everyone shakes everyone else’s hand exactly once, how
+#many handshakes take place?
+
+#8. There are variety of special hands that one can be dealt in poker. For each of the following
+#types of hands, count the number of hands that have that type.
+#(a) Flush. The hand contains five cards all of the same suit.
+#(b) Straight. The five cards have consecutive numerical values, such as 7-8-9-10-jack. Treat
+#the ace as being higher than the king but not less than the 2. The suits are irrelevant.
+#(c) Straight Flush. The hand is both a straight and a flush.
+#(d) Two Pair. The hand contains two different pairs and an unmatched fifth card.
+
+
+# Solutions
+#1. 2^6 = 64
+#2. 2^16 = 65536
+#3. 2^4 = 16
+#4. 5! = 120
+#5. 10!/(6!*4!) = 151200
+#6. 13C8 = 1287
+#7. 20C2 = 190
+#8. (a) 4^5 = 1024
+#(b) 10C5 * 4^5 = 2598960
+#(c) 10C5 * 4^5 = 2598960
+#(d) 10C2 * 9C2 * 8C2 * 7C2 * 6C2 = 2598960
+
+# Cardinality of a set
+# A set is a collection of objects. The cardinality of a set is the number of objects in the set.
+# The cardinality of a set is denoted by |𝐴|.
+# The cardinality of a set 𝐴 is the number of elements in 𝐴.
+
+# (n)k = n!/(n-k)!
+
--- R/377-hw8.r
+++ R/377-hw8.r
@@ -1,5 +1,37 @@
+# Print script as PDF
+pdf("377-hw8.pdf")
+cat("
+
+# Sampling Distribution
 # 4.79
-# Sampling Distribution of the Sample Mean
-ex479a <- 1 - pnorm(1.03)
-ex479b <- pnorm(1.03) - pnorm(-1.03)
-ex479c <- pnorm(1.03)
+ex479a <- pnorm(960, 930, 130 / sqrt(20)) - pnorm(900, 930, 130 / sqrt(20))
+ex479b <- 1 - pnorm(960, 930, 130 / sqrt(20))
+ex479c <- qnorm(.90, 930, 130 / sqrt(20))
+
+# 4.82
+ex482a <- 1 - pnorm(7, 5, 1.3)
+ex482b <- pnorm(5.5, 5, 1.3 / sqrt(500))
+
+# 4.83
+ex483a <- 1 - pnorm(2.7, 2.1, 0.3)
+ex483b <- qnorm(.75, 2.1, 0.3)
+ex483c <- qnorm(.95, 0, 0.3)
+ex483c2 <- 2.7 - ex483c
+ 
+# 4.84
+n <- 150
+u <- 2.1
+o <- 0.3
+
+ex484a <- n * u
+ex484b <- sqrt(n) * o
+
+# 4.85
+n <- 200
+u <- 95
+o <- 35
+
+ex485a <- n * u
+ex485b <- sqrt(n) * o
+ex485c <- 1 - pnorm(20000, 19000, 495)
+", file = "377-hw8.pdf", append = TRUE)
