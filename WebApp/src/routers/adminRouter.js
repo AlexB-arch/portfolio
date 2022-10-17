9190eb5638f1ecdae13a25baf2160f33e80ce8a8
Alexander Burgos <67289166+AlexB-arch@users.noreply.github.com>
2022-10-17 18:12:05 -0500

    Repo update

--- WebApp/src/routers/adminRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const debug = require('debug')('app:adminRouter');
-const users = require('../data/users.json'); //path that contains user data.
-
-const adminRouter = express.Router();
-
-module.exports = adminRouter;
