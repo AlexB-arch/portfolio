673664657867c3f7cf21da4085287c2d0b0cf83c
AlexB <axb19c@acu.edu>
2022-11-07 14:30:53 -0600

    Some commit from the other computer

---
+++ R/377-hw10.r
@@ -0,0 +1,19 @@
+# MATH 377 Homework 10
+
+# 5.16
+# (a)
+za <- (25.9 - 26) / (7.6/sqrt(50))
+Pa <- qnorm(.95)
+# (b)
+zb <- abs(24 - 26) / (7.6/sqrt(50))
+Pb <- pnorm(Pa - zb)
+# (c)
+zc <- abs(24 - 26) / (7.6/sqrt(100))
+Pc <- pnorm(Pa - zc)
+
+# 5.17
+# (a)
+
+# (b)
+
+# (c)
