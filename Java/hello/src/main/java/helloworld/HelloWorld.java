83e7e0b23e3b6a5bf88f1e48c30e85ecf6f21652
AlexB <axb19c@acu.edu>
2023-08-30 13:47:28 -0500

    New Java projects.

---
+++ Java/hello/src/main/java/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+ *  Author : axb19c Alex Burgos
+ *  Class  : CS374
+ *  Task   : Week 01 Check - Java Hello World
+ *
+ *  Compile: javac HelloWorld.java
+ *  Execute: java HelloWorld
+ *
+ *  Prints "Hello, World". Tevia says, this is everyone's first program.
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ *
+ ******************************************************************************/
+package helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
---
+++ React/cs440/traveltracker/src/components/button-primary-enabled.js
@@ -0,0 +1,12 @@
+import buttonimage from '@/assets/button-primary-enabled.svg';
+
+function PrimaryButtonEnabled() {
+  return (
+    <div>
+        <img src={buttonimage} alt="Primary Button Enabled" />
+    </div>
+    
+  );
+}
+
+export default PrimaryButtonEnabled;
