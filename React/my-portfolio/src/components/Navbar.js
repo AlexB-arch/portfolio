eadb4361618947253d59a704da2ccfb9c521a2e2
Alexander Burgos <alexanderburgos@AlexandersMBP2.localdomain>
2024-02-02 22:04:57 -0600

    Adds heroicons package.

--- React/my-portfolio/src/components/Navbar.js
+++ React/my-portfolio/src/components/Navbar.js
@@ -1,4 +1,6 @@
 import SearchBox from "./SearchBox";
+import React from "react";
+import { HomeIcon } from "@heroicons/react/20/solid";
 
 export default function Navbar() {
 	return (
@@ -6,10 +8,8 @@ export default function Navbar() {
             <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
                 <p className="flex items-center justify-center gap-2">
                     <h1 className="text-2xl font-bold text-black">My Portfolio</h1>
-                    <a href="#Home">Home</a>
-                    <a href="#About">About</a>
-                    <a href="#Projects">Projects</a>
-                    <a href="#Contact">Contact</a>
+                    <HomeIcon className="w-6 h-6" />
+                    
                 </p>
 
                 <section className="flex items-center gap-3">
